80762dccb092fbe3e05518956139569ebca06e3a
roshan <srma.rosn@gmail.com>
2022-02-12 16:51:31 +0545

    swagger connect

--- APIS/settings.py
+++ APIS/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
-    'rest_framework'
+    'rest_framework',
+    'rest_framework_swagger',
+    'drf_yasg2'
 ]
 
 MIDDLEWARE = [
--- APIS/urls.py
+++ APIS/urls.py
@@ -16,9 +16,27 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from rest_framework import permissions
+from drf_yasg2.views import get_schema_view
+from drf_yasg2 import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Coding API",
+        default_version = 'v1',
+        description = "Test description",
+        terms_of_serivice ='htpps://www.google.com/policies/terms/',
+        contact = openapi.Contact(email="contact@sanippets.local"),
+        license= openapi.License(name="BSD License"),
+
+    ),
+    public = True,
+    permission_classes = (permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('' ,include('home.urls'))
+    path('' ,include('home.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),name='schema-swagger-ui')
     
 ]
